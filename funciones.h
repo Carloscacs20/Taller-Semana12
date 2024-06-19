4fd0f86de2a90c9c6fd99acc1c99e4bbc8f874cb
Carloscacs20 <167730021+Carloscacs20@users.noreply.github.com>
2024-06-19 08:35:03 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -4,3 +4,4 @@ void listarPeliculas(char peliculas[][4][40]);
 void buscarporNombre(char peliculas[][4][40]);
 void buscarporGenero(char peliculas[][4][40]);
 void comprarTicket(char peliculas[][4][40], double precio[], char clientes[][2][40], int reserva[][4]);
+void verCompras(char peliculas[][4][40], double precio[], char clientes[][2][40], int reserva[][4]);
